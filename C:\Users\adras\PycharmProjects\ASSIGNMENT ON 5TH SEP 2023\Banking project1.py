746767a06b8b1fe74ba75fa58dc29ede51a20ca6
adithyasanka1 <155262067+adithyasanka1@users.noreply.github.com>
2024-01-31 02:47:50 +0530

    Update C:\Users\adras\PycharmProjects\ASSIGNMENT ON 5TH SEP 2023\Banking project1.py
    Python Banking Project

--- C:\Users\adras\PycharmProjects\ASSIGNMENT ON 5TH SEP 2023\Banking project1.py
+++ C:\Users\adras\PycharmProjects\ASSIGNMENT ON 5TH SEP 2023\Banking project1.py
@@ -1,1 +1,30 @@
+class BankAccount:
+    def __init__(self):
+        self.balance=0
+        print("Hello welcome to Lakshmi Bank")
+    def login(self):
+        account_login=int(input("Enter your account number:"))
+    def password(self):
+        account_password=int(input("Enter your password:"))
+        print("Login successful")
+    def deposit(self):
+        amount=float(input("Enter the amount you wish to deposit:"))
+        self.balance+=amount
+        print("\n Amount Deposited:",amount)
+    def withdrawal(self):
+        amount=float(input("Enter the amount you wish to withdraw:"))
+        if self.balance>=amount:
+            self.balance-=amount
+            print("\n Amount you have withdrawn:")
+        else:
+            print("\nSorry insufficient account balance")
+    def display(self):
+        print("\n Net available balance",self.balance)
+
+ba1=BankAccount()
+ba1.login()
+ba1.password()
+ba1.deposit()
+ba1.withdrawal()
+ba1.display()
 
